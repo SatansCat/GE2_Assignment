d7259ce6a55b5d362e2eb024d29a79f57b29485a
SatansCat <endamkeane@gmail.com>
2020-05-20 22:45:31 +0100

    Added Fourth Scene

---
+++ Assignment2/Assets/Scripts/Explode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public List<GameObject> Ships = new List<GameObject>();
+    public GameObject mainExplosion;
+    public GameObject ring;
+    public GameObject pointLight;
+
+    
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Sentient")
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            mainExplosion.SetActive(true);
+            ring.SetActive(true);
+            pointLight.SetActive(true);
+
+            foreach(GameObject s in Ships)
+            {
+                s.GetComponent<Pursue>().enabled = false;
+                s.GetComponent<Seek>().enabled = true;
+
+
+            }
+        }
+    }
+}
