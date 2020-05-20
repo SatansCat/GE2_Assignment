1776d35dc15f3d0760e9441df77834e456787a4a
SatansCat <endamkeane@gmail.com>
2020-05-20 19:00:37 +0100

    Finished first scene

---
+++ Assignment2/Assets/Scripts/AnimatorCalls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimatorCalls : MonoBehaviour
+{
+
+    public Animator sceneFade;
+
+    private int targetLvl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeSceneOut(int levelToLoad)
+    {
+        sceneFade.SetTrigger("FadeOut");
+        targetLvl = levelToLoad;
+    }
+
+    private void OnFadeComplete()
+    {
+        SceneManager.LoadScene(targetLvl);
+    }
+}
---
+++ Assignment2/Assets/Scripts/SpawnShips.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnShips : MonoBehaviour
+{
+    public GameObject Sentient;
+    public GameObject ShipContainer;
+    public GameObject launchLight;
+    public AnimatorCalls lvlChangeScript;
+    
+
+    public int NumShips;
+    public List<GameObject> Targets = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        bool trigger = false;
+        yield return new WaitForSeconds(2f);
+        for(int i = 0; i < NumShips; i++)
+        {
+            trigger = !trigger;
+            GameObject ship = Instantiate(Sentient, ShipContainer.transform, false);
+            ship.GetComponent<Seek>().targetGameObject = Targets[Random.Range(0, Targets.Count)];
+            launchLight.SetActive(trigger);
+            yield return new WaitForSeconds(.1f);
+        }
+        yield return new WaitForSeconds(3f);
+        lvlChangeScript.FadeSceneOut(1);
+        yield return new WaitForSeconds(3f);
+    }
+}
